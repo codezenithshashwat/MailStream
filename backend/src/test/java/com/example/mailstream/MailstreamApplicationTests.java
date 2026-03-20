ee1e08202a4ef58bfb0d1b7ffaa322593ff53748
Shashwat Trivedi <shashwattrivedi.156@gmail.com>
2026-03-21 01:12:43 +0530

    Initial commit (docker infrastructure + spring boot configuration and project setup)

---
+++ backend/src/test/java/com/example/mailstream/MailstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mailstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

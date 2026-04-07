46cf13234673464c4599dbf742f125665dcb4b48
Shashwat Trivedi <shashwattrivedi.156@gmail.com>
2026-04-08 00:18:20 +0530

    Bug fixed, the final core of this proejct is ready, sending mail testing done successfully

--- backend/src/main/java/com/example/mailstream/service/CampaignOrchestrator.java
+++ backend/src/main/java/com/example/mailstream/service/CampaignOrchestrator.java
@@ -32,7 +32,7 @@ public class CampaignOrchestrator {
     }
 
     // kafkalistener annotation creates a thread in the background that constantly listens to the tpoic.
-    @KafkaListener(topics= "campaign-initiation", groupId= "mailstream-group")
+    @KafkaListener(topics= "campaign_event", groupId= "mailstream-group")
     public void listenForNewCampaigns(String campaignIdStr){
         System.out.println("CONSUMER (Orchestrator): Just received Campaign ID [" + campaignIdStr + "] from Kafka!");
         System.out.println("️CONSUMER (Orchestrator): I will now start fetching subscribers from the database in the background...");
